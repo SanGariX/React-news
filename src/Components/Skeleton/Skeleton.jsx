e0e13dbf41f025107d902d8db86f7b6349468ce7
Horison <denya.garkusha.12@gmail.com>
2025-01-04 16:39:00 +0200

    skeletor for list and banner

---
+++ src/Components/Skeleton/Skeleton.jsx
@@ -0,0 +1,27 @@
+ import styles from './skeleton.module.css'
+const Skeleton = ({ count = 1, type = 'banner' }) => {
+	return (
+		<>
+			{count > 1 ? (
+				<div className={styles.wrapper}>
+					<ul className={styles.list}>
+						{[...Array(count)].map((_, idx) => (
+							<li
+								key={idx}
+								className={type === 'banner' ? styles.banner : styles.item}
+							></li>
+						))}
+					</ul>
+				</div>
+			) : (
+				<div className={styles.wrapper}>
+					<li
+						className={type === 'banner' ? styles.banner : styles.item}
+					></li>
+				</div>
+			)}
+		</>
+	)
+}
+
+export default Skeleton

deb2d099de169bce892f0fb722292eac0740972e
Horison <denya.garkusha.12@gmail.com>
2025-01-04 14:06:18 +0200

    0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
+import Home from './pages/home/Home'
 
-import AppRoutes from './routes/AppRoutes'
 function App() {
-	return <AppRoutes />
+	return <Home/>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'modern-normalize'
-import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter>
 			<App />
-		</BrowserRouter>
 	</StrictMode>
 )
